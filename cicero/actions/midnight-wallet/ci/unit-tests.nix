d6140c635edd1e910cc9403709c5336af6634718
Enrique Rodríguez <enrique.rodriguez@iohk.io>
2022-05-02 11:55:53 -0300

    [Chore] Set npm required credentials (#75)

--- cicero/actions/midnight-wallet/ci/unit-tests.nix
+++ cicero/actions/midnight-wallet/ci/unit-tests.nix
@@ -17,6 +17,15 @@
       actionLib.simpleJob
       (actionLib.common.task
         check-formatting.value."midnight-wallet/ci/check-formatting")
+
+      rec {
+        template = std.data-merge.append [{
+          destination = "secrets/npm_auth";
+          data = env.NPM_AUTH_TOKEN;
+        }];
+        env.NPM_AUTH_TOKEN = ''{{with secret "kv/data/cicero/nexus"}}{{.Data.data.token}}{{end}}'';
+      }
+
       (std.script "bash" ''
         sbt test
       '')
