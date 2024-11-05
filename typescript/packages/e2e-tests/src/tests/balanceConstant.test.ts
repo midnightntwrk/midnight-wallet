7284e3b2ba3648577f78c7d957ba5c1feecc4619
Monika Jaššová <monika.jassova@iohk.io>
2024-11-05 19:06:52 +0100

    [PM-11243] test: add balance healtchecks (#565)
    * test: add balance healthecks
    
    * update yarn.lock
    
    * use sync cache
    
    * unskip tests

---
+++ typescript/packages/e2e-tests/src/tests/balanceConstant.test.ts
@@ -0,0 +1,112 @@
+import { Resource, WalletBuilder } from '@midnight-ntwrk/wallet';
+import { TestContainersFixture, useTestContainersFixture } from './test-fixture';
+import { nativeToken, NetworkId } from '@midnight-ntwrk/zswap';
+import { closeWallet, provideWallet, saveState, waitForSync, walletStateTrimmed } from './utils';
+import { Wallet } from '@midnight-ntwrk/wallet-api';
+import { logger } from './logger';
+import { exit } from 'node:process';
+import { readFileSync, writeFileSync } from 'node:fs';
+
+/**
+ * Tests checking balance is constant
+ *
+ * @group devnet
+ * @group testnet
+ */
+
+describe('Balance constant', () => {
+  if (process.env.SEED_STABLE === undefined) {
+    logger.info('SEED_STABLE not set');
+    exit(1);
+  }
+  const getFixture = useTestContainersFixture();
+  const seed = process.env.SEED_STABLE;
+  const nativeTokenHash = '02000000000000000000000000000000000000000000000000000000000000000001';
+  const nativeTokenHash2 = '02000000000000000000000000000000000000000000000000000000000000000002';
+  const expectedDustBalance = 100_000_000n;
+  const expectedTokenOneBalance = 25n;
+  const expectedTokenTwoBalance = 50n;
+  const filename = `./.sync_cache/stable-${seed.substring(seed.length - 7)}-${TestContainersFixture.deployment}.state`;
+  const timeout = 180_000;
+
+  let wallet: Wallet & Resource;
+  let restoredWallet: Wallet & Resource;
+
+  beforeEach(async () => {
+    const fixture = getFixture();
+    let networkId: NetworkId;
+    switch (TestContainersFixture.network) {
+      case 'undeployed':
+        networkId = NetworkId.Undeployed;
+        break;
+      case 'devnet':
+        networkId = NetworkId.DevNet;
+        break;
+      case 'testnet':
+        networkId = NetworkId.TestNet;
+        break;
+    }
+
+    wallet = await WalletBuilder.buildFromSeed(
+      fixture.getIndexerUri(),
+      fixture.getIndexerWsUri(),
+      fixture.getProverUri(),
+      fixture.getNodeUri(),
+      seed,
+      networkId,
+      'info',
+    );
+
+    restoredWallet = await provideWallet(filename, seed, networkId, fixture);
+  }, timeout);
+
+  afterEach(async () => {
+    await saveState(restoredWallet, filename);
+    await closeWallet(wallet);
+    await closeWallet(restoredWallet);
+  }, timeout);
+
+  test(
+    'Balance is constant when syncing from 0 @healthcheck',
+    async () => {
+      allure.tag('healthcheck');
+      allure.tms('PM-13614', 'PM-13614');
+      allure.epic('Headless wallet');
+      allure.feature('Balance');
+      allure.story('Balance constant when syncing from 0');
+
+      wallet.start();
+      const syncedState = await waitForSync(wallet);
+      expect(syncedState.balances[nativeToken()] ?? 0n).toBe(expectedDustBalance);
+      expect(syncedState.balances[nativeTokenHash] ?? 0n).toBe(expectedTokenOneBalance);
+      expect(syncedState.balances[nativeTokenHash2] ?? 0n).toBe(expectedTokenTwoBalance);
+      expect(syncedState.availableCoins.length).toBe(3);
+      expect(syncedState.pendingCoins.length).toBe(0);
+      expect(syncedState.coins.length).toBe(3);
+      expect(syncedState.transactionHistory.length).toBe(2);
+    },
+    timeout,
+  );
+
+  test(
+    'Balance is constant when syncing from a restored state @healthcheck',
+    async () => {
+      allure.tag('healthcheck');
+      allure.tms('PM-13615', 'PM-13615');
+      allure.epic('Headless wallet');
+      allure.feature('Wallet state');
+      allure.story('Balance constant');
+
+      restoredWallet.start();
+      const syncedState = await waitForSync(restoredWallet);
+      expect(syncedState.balances[nativeToken()] ?? 0n).toBe(expectedDustBalance);
+      expect(syncedState.balances[nativeTokenHash] ?? 0n).toBe(expectedTokenOneBalance);
+      expect(syncedState.balances[nativeTokenHash2] ?? 0n).toBe(expectedTokenTwoBalance);
+      expect(syncedState.availableCoins.length).toBe(3);
+      expect(syncedState.pendingCoins.length).toBe(0);
+      expect(syncedState.coins.length).toBe(3);
+      expect(syncedState.transactionHistory.length).toBe(2);
+    },
+    timeout,
+  );
+});
--- typescript/packages/e2e-tests/src/tests/fundTestWallets.test.ts
+++ typescript/packages/e2e-tests/src/tests/fundTestWallets.test.ts
@@ -2,7 +2,14 @@ import { firstValueFrom } from 'rxjs';
 import { Resource, WalletBuilder } from '@midnight-ntwrk/wallet';
 import { TestContainersFixture, useTestContainersFixture } from './test-fixture';
 import { nativeToken, NetworkId } from '@midnight-ntwrk/zswap';
-import { closeWallet, waitForFinalizedBalance, waitForPending, waitForSync, walletStateTrimmed } from './utils';
+import {
+  closeWallet,
+  waitForFinalizedBalance,
+  waitForPending,
+  waitForSync,
+  waitForTxInHistory,
+  walletStateTrimmed,
+} from './utils';
 import { Wallet } from '@midnight-ntwrk/wallet-api';
 import { logger } from './logger';
 import { exit } from 'node:process';
@@ -19,19 +26,26 @@ describe('Token transfer', () => {
     logger.info('SEED env var not set');
     exit(1);
   }
+  if (process.env.SEED_STABLE === undefined) {
+    logger.info('SEED_STABLE env var not set');
+    exit(1);
+  }
   const getFixture = useTestContainersFixture();
   const seedFunded = process.env.SEED;
+  const seedStable = process.env.SEED_STABLE;
   const timeout = 3_600_000;
   const outputValue = 100_000_000n;
   const nativeTokenValue = 25n;
+  const nativeTokenValue2 = 50n;
   const nativeTokenHash = '02000000000000000000000000000000000000000000000000000000000000000001';
+  const nativeTokenHash2 = '02000000000000000000000000000000000000000000000000000000000000000002';
 
   let walletFunded: Wallet & Resource;
   let fixture: TestContainersFixture;
+  let networkId: NetworkId;
 
   beforeEach(async () => {
     fixture = getFixture();
-    let networkId: NetworkId;
     switch (TestContainersFixture.network) {
       case 'undeployed':
         networkId = NetworkId.Undeployed;
@@ -116,6 +130,84 @@ describe('Token transfer', () => {
       for (const address of addresses) {
         await sendTx(address);
       }
+    },
+    timeout,
+  );
+
+  test(
+    'Is working for preparing the stable wallet',
+    async () => {
+      const walletStable = await WalletBuilder.buildFromSeed(
+        fixture.getIndexerUri(),
+        fixture.getIndexerWsUri(),
+        fixture.getProverUri(),
+        fixture.getNodeUri(),
+        seedStable,
+        networkId,
+        'info',
+      );
+
+      walletStable.start();
+      const addressStable = (await firstValueFrom(walletStable.state())).address;
+      await closeWallet(walletStable);
+
+      await waitForSync(walletFunded);
+      const initialState = await firstValueFrom(walletFunded.state());
+      const initialBalance = initialState.balances[nativeToken()] ?? 0n;
+      const initialBalanceNative = initialState.balances[nativeTokenHash] ?? 0n;
+      const initialBalanceNative2 = initialState.balances[nativeTokenHash2] ?? 0n;
+      logger.info(`Wallet 1: ${initialBalance} tDUST`);
+      logger.info(`Wallet 1: ${initialBalanceNative} ${nativeTokenHash}`);
+      logger.info(`Wallet 1: ${initialBalanceNative2} ${nativeTokenHash2}`);
+      logger.info(`Wallet 1 available coins: ${initialState.availableCoins.length}`);
+      logger.info(
+        `Sending ${
+          outputValue / 1_000_000n
+        } tDUST and ${nativeTokenValue} ${nativeTokenHash} to address: ${addressStable}`,
+      );
+
+      const outputsToCreate = [
+        {
+          type: nativeToken(),
+          amount: outputValue,
+          receiverAddress: addressStable,
+        },
+        {
+          type: nativeTokenHash,
+          amount: nativeTokenValue,
+          receiverAddress: addressStable,
+        },
+      ];
+
+      const outputsToCreate2 = [
+        {
+          type: nativeTokenHash2,
+          amount: nativeTokenValue2,
+          receiverAddress: addressStable,
+        },
+      ];
+
+      const txToProve = await walletFunded.transferTransaction(outputsToCreate);
+      const provenTx = await walletFunded.proveTransaction(txToProve);
+      const id = await walletFunded.submitTransaction(provenTx);
+      logger.info('Transaction id: ' + id);
+      await waitForTxInHistory(id, walletFunded);
+
+      logger.info(`Sending ${nativeTokenValue2} ${nativeTokenHash2} to address: ${addressStable}`);
+      const txToProve2 = await walletFunded.transferTransaction(outputsToCreate2);
+      const provenTx2 = await walletFunded.proveTransaction(txToProve2);
+      const id2 = await walletFunded.submitTransaction(provenTx2);
+      logger.info('Transaction id: ' + id2);
+      await waitForTxInHistory(id2, walletFunded);
+      const finalState = await waitForSync(walletFunded);
+      logger.info(walletStateTrimmed(finalState));
+      expect(finalState.balances[nativeToken()] ?? 0n).toBeLessThan(initialBalance - outputValue);
+      expect(finalState.balances[nativeTokenHash] ?? 0n).toBe(initialBalanceNative - nativeTokenValue);
+      expect(finalState.balances[nativeTokenHash2] ?? 0n).toBe(initialBalanceNative2 - nativeTokenValue2);
+      expect(finalState.pendingCoins.length).toBe(0);
+      expect(finalState.transactionHistory.length).toBeGreaterThanOrEqual(initialState.transactionHistory.length + 2);
+
+      // TO-DO: contract deploy and call, obtaining minted token from contract
     },
     timeout,
   );
