fa1ba1682424f825e8401e88a5d5cc5bb6cc067e
Monika Jaššová <monika.jassova@iohk.io>
2024-11-07 12:19:55 +0100

    chore: add conversion script between seed and mnemonics (#568)

---
+++ typescript/packages/e2e-tests/scripts/conversion.mjs
@@ -0,0 +1,39 @@
+#!/usr/bin/env -S node  --experimental-specifier-resolution=node
+/* eslint-disable import/no-extraneous-dependencies */
+
+import * as KeyManagement from "@cardano-sdk/key-management"
+
+const mnemonics = [
+    'abandon',
+    'abandon',
+    'abandon',
+    'abandon',
+    'abandon',
+    'abandon',
+    'abandon',
+    'abandon',
+    'abandon',
+    'abandon',
+    'abandon',
+    'abandon',
+    'abandon',
+    'abandon',
+    'abandon',
+    'abandon',
+    'abandon',
+    'abandon',
+    'abandon',
+    'abandon',
+    'abandon',
+    'abandon',
+    'absurd',
+    'fetch'
+  ];
+
+  const seed = KeyManagement.util.mnemonicWordsToEntropy(mnemonics)
+  console.log(seed);
+
+  const seed2 = '0000000000000000000000000000000000000000000000000000000000000042';
+  const mnemonics2 = KeyManagement.util.entropyToMnemonicWords(seed2);
+  console.log(mnemonics2);
+
