5a608ec6f0f03901b47a2cd99503c99384626e12
Ian Gregson <ian.gregson@shielded.io>
2025-10-24 11:53:23 +0200

    [PM-20038] Improve npm publishing (#782)

--- packages/capabilities/rollup.config.mjs
+++
@@ -1,6 +0,0 @@
-import { dirname } from 'node:path';
-import { fileURLToPath } from 'node:url';
-import configure from '../../rollup.config.mjs';
-import packageJson from './package.json' with { type: 'json' };
-
-export default configure(dirname(fileURLToPath(import.meta.url)), packageJson);
