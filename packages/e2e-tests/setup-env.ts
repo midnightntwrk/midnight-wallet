0db86d2a9d5fd779f521d37d01ebc31ea135dd0b
Ian Gregson <ian.gregson@shielded.io>
2025-11-19 12:07:07 +0100

    [PM-20624] Fix and improve failing GitHub workflows

--- packages/e2e-tests/setup-env.ts
+++ packages/e2e-tests/setup-env.ts
@@ -12,5 +12,3 @@
 // limitations under the License.
 import dotenv from 'dotenv';
 dotenv.config({ path: '.env', override: true });
-console.log('NETWORK', process.env['NETWORK']);
-console.log('DEPLOYMENT', process.env['DEPLOYMENT']);
