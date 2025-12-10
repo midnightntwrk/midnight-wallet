e67e7534b469dd5020e68d1d4b2db3aa4a74e754
Agron Murtezi <agron.murtezi@iohk.io>
2025-12-10 13:23:36 +0100

    fix: fixes ledger postBlockUpdate param change

--- packages/dust-wallet/src/Simulator.ts
+++ packages/dust-wallet/src/Simulator.ts
@@ -92,6 +92,16 @@ export class Simulator {
   ): Either.Either<[{ blockNumber: bigint; blockHash: string }, SimulatorState], LedgerOps.LedgerError> {
     return LedgerOps.ledgerTry(() => {
       blockFullness = blockFullness ?? tx.cost(simulatorState.ledger.parameters);
+
+      const detailedBlockFullness = simulatorState.ledger.parameters.normalizeFullness(blockFullness);
+      const computedBlockFullness = Math.max(
+        detailedBlockFullness.readTime,
+        detailedBlockFullness.computeTime,
+        detailedBlockFullness.blockUsage,
+        detailedBlockFullness.bytesWritten,
+        detailedBlockFullness.bytesChurned,
+      );
+
       const blockNumber = blockContext.secondsSinceEpoch;
       const blockTime = DateOps.secondsToDate(blockNumber);
       const verifiedTransaction = tx.wellFormed(simulatorState.ledger, strictness, blockTime);
@@ -100,7 +110,7 @@ export class Simulator {
 
       const newSimulatorState = {
         ...simulatorState,
-        ledger: newLedgerState.postBlockUpdate(blockTime, blockFullness),
+        ledger: newLedgerState.postBlockUpdate(blockTime, detailedBlockFullness, computedBlockFullness),
         lastTx: tx,
         lastTxResult: txResult,
         lastTxNumber: blockNumber,
