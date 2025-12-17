a15657b4cb634a13e946b581d7c59e578c0632ae
Jegor Sidorenko <jegor.sidorenko@shielded.io>
2025-12-17 12:34:14 +0200

    Use fetching with a retry

--- packages/prover-client/src/proof-worker.ts
+++ packages/prover-client/src/proof-worker.ts
@@ -17,6 +17,17 @@ import { parentPort, workerData } from 'worker_threads';
 const s3 = 'https://midnight-s3-fileshare-dev-eu-west-1.s3.eu-west-1.amazonaws.com';
 const ver = 6;
 
+const fetchWithRetry = async (url: string, retries = 3): Promise<Response> => {
+  for (let i = 0; i < retries; i++) {
+    try {
+      return await fetch(url);
+    } catch (e) {
+      console.log('Failed to fetch at attempt', i + 1, url, e);
+    }
+  }
+  throw new Error(`Failed to fetch ${url} after ${retries} attempts`);
+};
+
 const cache = new Map<string, ProvingKeyMaterial | Uint8Array>();
 
 const keyMaterialProvider = {
@@ -30,62 +41,48 @@ const keyMaterialProvider = {
     if (pth === undefined) {
       return undefined;
     }
+
     if (cache.has(pth)) {
-      console.log('Using cached key material for', pth);
       return cache.get(pth) as ProvingKeyMaterial;
     }
 
-    const pk = await fetch(`${s3}/${pth}.prover`);
-    const vk = await fetch(`${s3}/${pth}.verifier`);
-    const ir = await fetch(`${s3}/${pth}.bzkir`);
+    const pk = await fetchWithRetry(`${s3}/${pth}.prover`);
+    const vk = await fetchWithRetry(`${s3}/${pth}.verifier`);
+    const ir = await fetchWithRetry(`${s3}/${pth}.bzkir`);
+
     const result = {
       proverKey: new Uint8Array(await pk.arrayBuffer()),
       verifierKey: new Uint8Array(await vk.arrayBuffer()),
       ir: new Uint8Array(await ir.arrayBuffer()),
     };
     cache.set(pth, result);
+
     return result;
   },
   getParams: async (k: number): Promise<Uint8Array> => {
     const cacheKey = `params-${k}`;
     if (cache.has(cacheKey)) {
-      const result = cache.get(cacheKey) as Uint8Array;
-      console.log('Using cached params for', cacheKey, result.length);
-      return result;
+      return cache.get(cacheKey) as Uint8Array;
     }
-    console.log('Fetching params: ', `${s3}/bls_filecoin_2p${k}`);
-    let data;
-    let result; //= new Uint8Array(data);
 
-    try {
-      data = await fetch(`${s3}/bls_filecoin_2p${k}`, {
-        headers: {
-          Connection: 'keep-alive',
-        },
-      });
-      const blob = await data.blob();
-      result = new Uint8Array(await blob.arrayBuffer());
-      console.log('Params fetched: ', `${s3}/bls_filecoin_2p${k}`);
-      data = null;
-    } catch (e) {
-      console.log('Failed to fetch params', `${s3}/bls_filecoin_2p${k}`, e);
-      throw e;
-    }
+    const data = await fetchWithRetry(`${s3}/bls_filecoin_2p${k}`);
+    const result = new Uint8Array(await data.arrayBuffer());
     cache.set(cacheKey, result);
+
     return result;
   },
 };
 const wasmProver = provingProvider(keyMaterialProvider);
 
-console.log('Worker started', workerData);
-const [op, args]: ['check' | 'prove', any[]] = workerData;
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+const [op, args]: ['check' | 'prove', Array<never>] = workerData;
 // we handle polymorphic data here
 // @ts-nocheck
 if (op === 'check') {
   const [a, b] = args;
   const result = await wasmProver.check(a, b);
   parentPort!.postMessage(result);
-} else {
+} else if (op === 'prove') {
   const [a, b, c] = args;
   const result = await wasmProver.prove(a, b, c);
   parentPort!.postMessage(result);
