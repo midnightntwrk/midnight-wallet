f69f45cc09905a27e5b44a6d4d95cc5b2a705143
Andrzej Kopeć <andrzej.kopec@shielded.io>
2026-02-13 13:10:05 +0100

    Fix lint

--- packages/docs-snippets/src/utils.ts
+++ packages/docs-snippets/src/utils.ts
@@ -89,10 +89,10 @@ export const initWalletWithSeed = async (
 };
 
 export const aFakeProvingProvider: ledger.ProvingProvider = {
-  check(serializedPreimage: Uint8Array, keyLocation: string): Promise<(bigint | undefined)[]> {
+  check(_serializedPreimage: Uint8Array, _keyLocation: string): Promise<(bigint | undefined)[]> {
     return Promise.resolve([]);
   },
-  prove(serializedPreimage: Uint8Array, keyLocation: string, overwriteBindingInput?: bigint): Promise<Uint8Array> {
+  prove(_serializedPreimage: Uint8Array, _keyLocation: string, _overwriteBindingInput?: bigint): Promise<Uint8Array> {
     return Promise.resolve(new Uint8Array(0));
   },
 };
