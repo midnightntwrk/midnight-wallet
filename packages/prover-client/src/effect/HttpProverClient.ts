eed535fc29a2f75d4134ef743e5d495f6f79c285
Agron Murtezi <agronmurtezi@gmail.com>
2026-06-26 12:54:34 +0200

    fix: fix lint issue

--- packages/prover-client/src/effect/HttpProverClient.ts
+++ packages/prover-client/src/effect/HttpProverClient.ts
@@ -144,7 +144,7 @@ class HttpProverClientImpl implements Context.Tag.Service<ProverClient> {
         Effect.flatMap((tx) => this.request(PROVE_TX_PATH, tx, 'Failed to prove')),
         Effect.runPromise,
       ),
-    lookupKey: async (_keyLocation: string): Promise<ledger.ProvingKeyMaterial | undefined> => undefined,
+    lookupKey: (_keyLocation: string): Promise<ledger.ProvingKeyMaterial | undefined> => Promise.resolve(undefined),
   });
 
   proveTransaction<S extends ledger.Signaturish, B extends ledger.Bindingish>(
