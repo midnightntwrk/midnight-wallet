9dc41d71e2f35689cc770f67f37663da40b57f43
Agron Murtezi <agronmurtezi@gmail.com>
2026-08-20 17:19:01 +0200

    feat(capabilities): validate at the version a transaction was authored for

--- packages/capabilities/src/validation/index.ts
+++ packages/capabilities/src/validation/index.ts
@@ -11,4 +11,5 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 export * from './blockData.js';
+export * from './preForkValidationService.js';
 export * from './validationService.js';
